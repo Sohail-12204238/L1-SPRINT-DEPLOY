014d7635268324cc0f6ca03892a51f75eab11176
cse-sohail-ashraf <cse.shaik.sohail.ashraf@gmail.com>
2026-05-01 14:45:13 +0530

    Live deploy fix

--- founderlink-frontend/src/api/axiosConfig.js
+++ founderlink-frontend/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8085';
+const API_BASE_URL = `http://${window.location.hostname}:8085`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
